46cb5345965edfd52976ec0286a68c51923f491b
Kenny The <kennythesman@gmail.com>
2016-08-14 19:05:18 +0800

    14/8/2016
    Testing global variable across script.

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,21 +8,31 @@ public class MainMenu : MonoBehaviour
     public GameObject playButton;
     public GameObject quitButton;
     public GameObject testLevel;
-    public GameObject acquisitionLevel;
-    public GameObject extinctionLevel;
+    public GameObject group1;
+    public GameObject group2;
+    public GameObject group3;
+    public GameObject group4;
     public GameObject yesQuit;
     public GameObject noQuit;
     public GameObject back;
     public Text quitConfirm;
+    static public int acquisitionCount = 0;
+    static public int extinctionCount = 0;
+    static public int[] acqMapOrder = { 2, 3, 4 };
+    static public int[] extMapOrder = { 5, 6, 7 };
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        Shuffle(acqMapOrder);
+        Shuffle(extMapOrder);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         testLevel.SetActive(false);
-        acquisitionLevel.SetActive(false);
-        extinctionLevel.SetActive(false);
+        group1.SetActive(false);
+        group2.SetActive(false);
+        group3.SetActive(false);
+        group4.SetActive(false);
         yesQuit.SetActive(false);
         noQuit.SetActive(false);
         back.SetActive(false);
@@ -40,8 +50,10 @@ public class MainMenu : MonoBehaviour
         playButton.SetActive(false);
         quitButton.SetActive(false);
         testLevel.SetActive(true);
-        acquisitionLevel.SetActive(true);
-        extinctionLevel.SetActive(true);
+        group1.SetActive(true);
+        group2.SetActive(true);
+        group3.SetActive(true);
+        group4.SetActive(true);
         back.SetActive(true);
     }
 
@@ -78,12 +90,25 @@ public class MainMenu : MonoBehaviour
     {
         quitConfirm.enabled = false;
         testLevel.SetActive(false);
-        acquisitionLevel.SetActive(false);
-        extinctionLevel.SetActive(false);
+        group1.SetActive(false);
+        group2.SetActive(false);
+        group3.SetActive(false);
+        group4.SetActive(false);
         yesQuit.SetActive(false);
         noQuit.SetActive(false);
         playButton.SetActive(true);
         quitButton.SetActive(true);
         back.SetActive(false);
+    }
+
+    public static void Shuffle<T>(T[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            T tmp = array[i];
+            int r = Random.Range(i, array.Length);
+            array[i] = array[r];
+            array[r] = tmp;
+        }
     }
 }
